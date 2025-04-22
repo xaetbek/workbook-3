90b98ad131b5ba4a2516c51c81267201a18bc906
Hayat A <xaetbek@gmail.com>
2025-04-22 11:18:03 -0700

    Create a new Java application named FamousQuotes.

---
+++ FamousQuotes/src/main/java/org/pluralsight/FamousQuote.java
@@ -0,0 +1,17 @@
+package org.pluralsight;
+
+public class FamousQuote {
+    String[] quotesList = {
+            "Always borrow money from a pessimist. He won’t expect it back. – Oscar Wilde (probably)",
+            "If you think nobody cares if you’re alive, try missing a couple of car payments. – Earl Wilson",
+            "I intend to live forever. So far, so good. – Steven Wright",
+            "I used to think I was indecisive, but now I’m not so sure. – Unknown",
+            "Why do they call it rush hour when nothing moves? – Robin Williams",
+            "Behind every great man is a woman rolling her eyes. – Jim Carrey",
+            "I can resist everything except temptation. – Oscar Wilde",
+            "My bed is a magical place where I suddenly remember everything I forgot to do. – Unknown",
+            "Some people graduate with honors, I am just honored to graduate. – Unknown",
+            "Never follow anyone else’s path. Unless you’re in the woods and you’re lost and you see a path. Then by all means, follow that path. – Ellen DeGeneres"
+    };
+
+}
---
+++ FamousQuotes/src/main/java/org/pluralsight/Main.java
@@ -0,0 +1,13 @@
+package org.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, select a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+
+
+    }
+}
